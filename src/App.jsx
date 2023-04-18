f0c7155df8acc6fab111e4000c10a8efcbeb66f1
Mendel Zirkind <mendelzirkindfresno@gmail.com>
2023-04-17 20:43:39 -0400

    added second product (red apple)

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,17 @@ function ProductsNew() {
       <h1>Potato Chips</h1>
       <h2>Crunchy Potato Chips</h2>
       <h3>$3.00</h3>
-      <img src="image.png" alt="Bag of Potato Chips" />
+      <img
+        src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQBSEc0SDsYSXsj97QTQtGeFcHhNnd_Y7AYZjETjsDICRc1hRRS9AdoUeJkx1AsM0A_kTo_g8n-ik19ZDow5aDhFLZxNnQkwgIXY67pfpPxc_4QRFQBRPFQeqdXa8XIKvbTT1E&usqp=CAc"
+        alt="Bag of Potato Chips"
+      />
+      <h1>Apple</h1>
+      <h2>Delicous Looking Red Apple</h2>
+      <h3>$2.00</h3>
+      <img
+        src="https://i5.walmartimages.com/asr/7320e63a-de46-4a16-9b8c-526e15219a12_3.e557c1ad9973e1f76f512b34950243a3.jpeg"
+        alt="Red Apple"
+      />
     </div>
   );
 }
